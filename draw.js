bf405169c22f33d7757bffeb020ff71b53056e27
Rawand Dargali <rdargali@yahoo.com>
2020-05-12 04:23:01 -0500

    canvas init

---
+++ draw.js
@@ -0,0 +1,13 @@
+const canvas = document.querySelector(".canvas");
+const ctx = canvas.getContext("2d");
+const scale = 10;
+
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
+
+var snake;
+
+(function setup() {
+  snake = new Snake();
+  snake.draw;
+})();
